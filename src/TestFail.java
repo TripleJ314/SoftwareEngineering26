5ae1467498bc4b792613405d8a9a36d6ec5eae8c
Jake J Jordan <jakejordan0314@gmail.com>
2026-09-03 13:47:26 -0400

    Fix the test code to pass syntax and indentation checks

--- src/TestFail.java
+++ src/TestFail.java
@@ -1,5 +1,5 @@
-public class Testfail {
+public class TestFail {
     public static void main(String[] args){
-     System.out.println("Hello, will this pass the tests?")
+        System.out.println("Hello, will this pass the tests?");
     }
 }
