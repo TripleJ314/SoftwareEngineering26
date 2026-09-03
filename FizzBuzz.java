eea13970ea029628f3ccf8a39b21edb18ed63d2f
Jake J Jordan <jakejordan0314@gmail.com>
2026-09-03 12:44:42 -0400

    Correct FizzBuzz indentation to match 4-space rule

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,13 +2,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
-            System.out.println("FizzBuzz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
-            System.out.println("Fizz");
+                System.out.println("Fizz");
             } else if (i % 5 == 0) {
-            System.out.println("Buzz");
+                System.out.println("Buzz");
             } else {
-            System.out.println(i);
+                System.out.println(i);
             }
         }
     }
