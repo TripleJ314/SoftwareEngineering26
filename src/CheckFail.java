c6846a316738b712ea2fe58348db97f06e80cf1c
Jake J Jordan <jakejordan0314@gmail.com>
2026-09-10 13:16:07 -0400

    Update CheckFail.java

--- src/CheckFail.java
+++ src/CheckFail.java
@@ -1,5 +1,5 @@
 public class CheckFail{
-    public static void main(String[ args) {
-     System.out.println("Will this work?")
+    public static void main(String[] args) {
+        System.out.println("Will this work?");
     }
 }
