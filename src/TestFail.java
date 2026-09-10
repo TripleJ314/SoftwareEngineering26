204f03d46fa24f74b98427790aad6a459f15275f
Jake J Jordan <jakejordan0314@gmail.com>
2026-09-10 11:27:58 -0400

    Fix TestFail.java to pass test Checks

--- src/TestFail.java
+++ src/TestFail.java
@@ -1,5 +1,5 @@
 public class TestFail {
-    public static void main(String[ args){
-     System.out.println("Hello, will this pass the tests?")
+    public static void main(String[] args){
+        System.out.println("Hello, will this pass the tests?");
     }
 }
